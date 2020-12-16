8fec342208e4bc7dd18efc191ccfe15b100c50ba
moonstoper <sjkachhap@outlook.com>
2020-12-16 23:49:17 +0530

    mongo key added /config

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+export const mongokey ="mongodb+srv://suraj-upi:5Jbqba4vUkeZNmSM@cluster0.m7isg.mongodb.net/upiprofile?retryWrites=true&w=majority";
+export const razor_key_id = "rzp_test_j9UI9PNWEZNmJN";
+export const razor_key_secret= "uooir37C3ZPC5xcKQzFdZ8fr";
