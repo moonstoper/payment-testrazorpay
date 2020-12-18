742539554d4d5571eb1a11d6ca823bb064f623a3
moonstoper <sjkachhap@outlook.com>
2020-12-18 06:02:09 +0530

    minors

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+export const mongokey ="mongodb+srv://suraj-upi:5Jbqba4vUkeZNmSM@cluster0.m7isg.mongodb.net/upiprofile?retryWrites=true&w=majority";
+//export const mongokey ="mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb";
+export const razor_key_id = "rzp_test_j9UI9PNWEZNmJN";
+export const razor_key_secret= "uooir37C3ZPC5xcKQzFdZ8fr";
+
+//secret keys
